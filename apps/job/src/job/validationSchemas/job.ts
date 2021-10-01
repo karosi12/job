c169321eaed7af62b1e209d2aa1409c49c7cfc1f
karosi12 <adekayor@gmail.com>
2021-10-01 12:55:26 +0100

    feat: validate job request payload

---
+++ apps/job/src/job/validationSchemas/job.ts
@@ -0,0 +1,68 @@
+import * as Joi from 'joi';
+
+const jobSchemas = {
+  createJobSchema: Joi.object().keys({
+    jobTitle: Joi.string()
+      .trim(true)
+      .required()
+      .regex(/^[A-Za-z]/)
+      .label('Job title')
+      .messages({ 'any.required': 'Job title is required' }),
+    description: Joi.string()
+      .trim(true)
+      .required()
+      .regex(/^[A-Za-z]/)
+      .label('Description')
+      .messages({ 'any.required': 'Description is required' }),
+    location: Joi.string()
+      .label('location')
+      .lowercase()
+      .required()
+      .valid('berlin', 'hamburg', 'remote')
+      .messages({ 'any.required': 'Location is required' }),
+  }),
+
+  updateJobSchema: Joi.object().keys({
+    id: Joi.string()
+      .guid({ version: 'uuidv4' })
+      .trim(true)
+      .required()
+      .label('id')
+      .messages({ 'any.required': 'id is required' }),
+    jobTitle: Joi.string()
+      .trim(true)
+      .required()
+      .regex(/^[A-Za-z]/)
+      .label('Job title')
+      .messages({ 'any.required': 'Job title is required' }),
+    description: Joi.string()
+      .trim(true)
+      .required()
+      .regex(/^[A-Za-z]/)
+      .label('Description')
+      .messages({ 'any.required': 'Description is required' }),
+    location: Joi.string()
+      .label('location')
+      .lowercase()
+      .required()
+      .valid('berlin', 'hamburg', 'remote')
+      .messages({ 'any.required': 'Location is required' }),
+  }),
+
+  paramSchema: Joi.object().keys({
+    id: Joi.string()
+      .guid({ version: 'uuidv4' })
+      .trim(true)
+      .required()
+      .label('id')
+      .messages({ 'any.required': 'id is required' }),
+  }),
+
+  validateOptions: {
+    abortEarly: false,
+    stripUnknown: true,
+    convert: true,
+  },
+};
+
+export { jobSchemas };
