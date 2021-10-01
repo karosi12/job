1abf52f6b522c7433ef05aac357e94e0c999abc7
karosi12 <adekayor@gmail.com>
2021-10-01 12:53:12 +0100

    feat: import job module

--- apps/job/src/app/app.module.ts
+++ apps/job/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-
+import { JobModule } from '../job/job.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [JobModule],
   controllers: [AppController],
   providers: [AppService],
 })
