195f7e1bf28001d1d4a85a67f225f6a10f8930ef
karosi12 <adekayor@gmail.com>
2021-10-01 12:55:48 +0100

    feat: update job module

--- apps/job/src/job/job.module.ts
+++ apps/job/src/job/job.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { JobController } from './job.controller';
+import { JobService } from './job.service';
 
-@Module({})
+@Module({
+  providers: [JobService],
+  controllers: [JobController],
+})
 export class JobModule {}
