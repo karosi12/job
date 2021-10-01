0e9484db65211d5eac156adf9fa80a836c84ef42
karosi12 <adekayor@gmail.com>
2021-10-01 14:04:33 +0100

    feat: import utils

--- apps/job/src/job/job.module.ts
+++ apps/job/src/job/job.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { Utils } from '../shared/utils';
 import { JobController } from './job.controller';
 import { JobService } from './job.service';
 
 @Module({
-  providers: [JobService],
+  providers: [JobService, Utils],
   controllers: [JobController],
 })
 export class JobModule {}
